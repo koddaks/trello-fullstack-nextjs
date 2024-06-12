15dd365c06e707b4c23b4aacea3c7b994f515786
koddaks <koss199710@gmail.com>
2024-06-12 16:44:33 +0300

    Folders Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className="text-sky-500">Hello</div>;
+  return <div className="text-sky-500">Hello Trello</div>;
 }
