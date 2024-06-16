f9d8ecc9294feafa8e916080d9e3fb8b9bc011dc
koddaks <koss199710@gmail.com>
2024-06-16 18:21:19 +0300

    make organizations route

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,8 @@
+import { OrganizationSwitcher } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+
+const OrganizationIdPage = () => {
+  return <div>Organization Page</div>;
+};
+
+export default OrganizationIdPage;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,32 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { auth, authMiddleware, clerkMiddleware, redirectToSignIn } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+export default authMiddleware({
+  publicRoutes: ['/'],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = '/select-org';
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSeletion = new URL(path, req.url);
+      return NextResponse.redirect(orgSeletion);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if( auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
+      const orgSeletion = new URL('/select-org', req.url);
+      return NextResponse.redirect(orgSeletion);
+    }
+  },
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
