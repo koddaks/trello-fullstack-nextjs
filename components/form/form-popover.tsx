da4f97df964f73b974770d7151dc26a60351ce6f
koddaks <koss199710@gmail.com>
2024-06-24 19:59:44 +0300

    Board Server Action

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -14,6 +14,9 @@ import { FormSubmit } from './form-submit';
 import { Button } from '@/components/ui/button';
 import { XIcon } from 'lucide-react';
 import { toast } from 'sonner';
+import { FormPicker } from './form-picker';
+import { ElementRef, useRef } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -28,22 +31,26 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<'button'>>(null);
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log(data);
       toast.success('Board created');
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.log(error);
       toast.error(error);
     },
   });
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string;
+    const image = formData.get('image') as string;
 
-    execute({ title });
-  }
+    execute({ title, image });
+  };
 
   return (
     <Popover>
@@ -57,7 +64,7 @@ export const FormPopover = ({
         <div className="textsm font-medium text-center text-neutral-600 pb-4">
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
@@ -67,7 +74,13 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
-            <FormInput id="title" label="Board title" type="text" errors={fieldErrors} />
+            <FormPicker id="image" errors={fieldErrors} />
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            />
           </div>
           <FormSubmit className="w-full">Create</FormSubmit>
         </form>
