6a6c83a72cc23fe7bf8619bc58af0285d6e32e9f
koddaks <koss199710@gmail.com>
2024-07-02 12:41:21 +0300

    List Header

--- actions/update-list/index.ts
+++ actions/update-list/index.ts
@@ -5,7 +5,7 @@ import { InputType, ReturnType } from './types';
 import { db } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
 import { createSafeAction } from '@/lib/create-safe-action';
-import { UpdateBoard } from './schema';
+import { UpdateList } from './schema';
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -16,14 +16,17 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  const { title, id } = data;
-  let board;
+  const { title, id, boardId } = data;
+  let list;
 
   try {
-    board = await db.board.update({
+    list = await db.list.update({
       where: {
         id,
-        orgId,
+        boardId,
+        board: {
+          orgId,
+        },
       },
       data: {
         title,
@@ -31,14 +34,14 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     });
   } catch (error) {
     return {
-      error: 'Failed to update board',
+      error: 'Failed to update list',
     };
   }
 
-  revalidatePath(`/board/${id}`);
+  revalidatePath(`/board/${boardId}`);
   return {
-    data: board,
+    data: list,
   };
 };
 
-export const updateBoard = createSafeAction(UpdateBoard, handler);
+export const updateList = createSafeAction(UpdateList, handler);
